44c12d40c4bb9224afe821fadf8eee44a5b64d39
Daniel Kelly <me@danielkelly.io>
2025-12-11 14:45:47 -0600

    7-getting-usage-data-with-the-ai-sdk

---
+++ 7-usage.ts
@@ -0,0 +1,9 @@
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+const result = await generateText({
+  model: google("gemini-2.5-flash"),
+  prompt: "Who is the most powerful super hero and why? Be concise.",
+});
+
+console.log(result.usage);
