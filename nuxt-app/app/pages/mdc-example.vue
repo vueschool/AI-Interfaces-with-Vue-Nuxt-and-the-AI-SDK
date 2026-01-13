d6e6f1776a30f7e2695133a559e9e3211012e700
Daniel Kelly <me@danielkelly.io>
2026-01-13 14:16:07 -0600

    21-how-to-display-chat-message-markdown-text-for-rich-formatting

---
+++ nuxt-app/app/pages/mdc-example.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const md = `**Hello world** This is a test of the MDC parser [google](https://google.com)`
+</script>
+
+<template>
+  <div class="p-10">
+    <MDCCached :cache-key="md" :value="md" />
+  </div>
+</template>
